50e8d125dfc1db5aebaee4579858453eddaaaa5a
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-01-20 14:57:12 +0100

    add form and weather components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Titles from "./Components/Titles";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
 
 class App extends React.Component {
   render() {
     return (
         <div>
-          <Titles/>
+            <Titles/>
+            <Form/>
+            <Weather/>
         </div>
     );
   }
---
+++ src/Components/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Weather Finder</h1>
+                <h3>Find out latest weather report of your favourite Cities</h3>
+            </div>
+        )
+    }
+}
+
+export default Weather;
