44325e53e0baf29453fb0397d3fca78f34c43cc5
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-01-22 09:31:49 +0100

    code improve, and add styling with boostrap.

--- src/App.js
+++ src/App.js
@@ -49,8 +49,22 @@ class App extends React.Component {
   render() {
     return (
         <div>
-            <Titles/>
-            <Form getWeather={this.getWeather}/>
+            <div className="wrapper">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col col-xs-5 title-container">
+                                <Titles/>
+                            </div>
+                            <div className="col col-xs-7 form-container">
+                                <Form getWeather={this.getWeather}/>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <Weather
                 temperature={this.state.temperature}
                 city={this.state.city}
--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Titles = () => (
     <div>
-        <h1>Weather Finder</h1>
-        <h3>Find out latest weather report of your favourite Cities</h3>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">Find out latest weather report of your favourite Cities</h3>
     </div>
 );
 
